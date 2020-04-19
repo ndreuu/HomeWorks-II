c7f5d341c87a997751131ab80fac200880a6647d
AndrewOleynikov <55163913+AndrewOleynikov@users.noreply.github.com>
2020-04-19 17:00:04 +0300

    Update Main.java

--- CarsClasses/main/Main.java
+++ CarsClasses/main/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	LadaPriora myPriora = new LadaPriora(200, 4, 2000000, "Russia","Lada Priora", 2010);
 	Volga myVolga = new Volga(100,4,1000000,"USSR","Volga","Black");
-        System.out.println(myPriora.getInfo());
-        System.out.println(myVolga.getInfo());
+	System.out.println(myPriora.getInfo());
+	System.out.println(myVolga.getInfo());
     }
 }
